f052b5e590cfa6a25c1393eb364cc73a0eedf793
vaibhav kottari <kottarivaibhav@gmail.com>
2024-05-17 19:12:48 +0530

    index added

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio_website/' // Ensure this matches your repository name
+  base: '/portfolio_website/', // Ensure this matches your repository name exactly
+  build: {
+    outDir: 'dist', // Ensure this matches your deploy directory
+    assetsDir: 'assets' // This can be customized as needed
+  }
 });
