c5747c0193bd4fdeae74cb11a9a2787bb59fcec6
vaibhav kottari <kottarivaibhav@gmail.com>
2024-08-04 23:40:33 +0530

    changes revised

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-
-import './App.css'
+import React from 'react';
+import './App.css';
 import Header from './components/Header';
 import BannerSection from './components/Banner';
 import ProjectSection from './components/Project';
@@ -7,36 +7,27 @@ import TechSection from './components/Technology';
 import AdditionalSkillSection from './components/AdditionalSkill';
 import AboutMeSection from './components/AboutSection';
 import ExperienceSection from './components/Experience';
+import CertificationCarousel from './components/CertificationCarousel'; // Import Carousel Component
 import Footer from './components/Footer';
 
-
 function App() {
-
-
   return (
     <>
-    <div className=''>
-    {/* Nav bar section*/}  
-    <Header />
-
-      <main className="relative mt-10  justify-normal left-4 right-1" >
-        {/* Intro Banner section */}
-        <BannerSection />
-        {/* Project Sections */}
-        <ProjectSection />
-        {/* Technologies Section */}
-        <TechSection />
-        {/* Additional skillset section */}
-        <AdditionalSkillSection />
-        {/* About Me Section */}
-        <AboutMeSection />
-        {/* Internship Experience Section */}
-        <ExperienceSection />
-      </main>
-      <Footer />
+      <div className=''>
+        <Header />
+        <main className="relative mt-10 justify-normal left-4 right-1">
+          <BannerSection />
+          <ProjectSection />
+          <TechSection />
+          <AdditionalSkillSection />
+          <AboutMeSection />
+          <ExperienceSection />
+          <CertificationCarousel /> {/* Add Carousel Component here */}
+        </main>
+        <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
