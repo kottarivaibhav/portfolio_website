3d576b6e3bc9ec79161c51fca2b9b6ca66c0f780
vaibhav kottari <kottarivaibhav@gmail.com>
2024-08-05 21:23:52 +0530

    changed folder name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio_website/', // Ensure this matches your repository name exactly
+  base: '/kottarivaibhav.github.io/', // Ensure this matches your repository name exactly
   build: {
     outDir: 'dist', // Ensure this matches your deploy directory
     assetsDir: 'assets' // This can be customized as needed
