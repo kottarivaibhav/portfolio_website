283bf0cdf78890505d0329e6fb477b64e792ed67
vaibhav kottari <kottarivaibhav@gmail.com>
2024-08-06 09:55:48 +0530

    firebase

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcIPL_Ni4tVO9dLRGXQVScuHS5nPrvNwo",
-  authDomain: "portfoliofeedback-fbc27.firebaseapp.com",
-  projectId: "portfoliofeedback-fbc27",
-  storageBucket: "portfoliofeedback-fbc27.appspot.com",
-  messagingSenderId: "417023353695",
-  appId: "1:417023353695:web:17558ac1998f841061ccea",
-  measurementId: "G-TZ6G2Q6F95"
+  apiKey: "--YOUR--KEY--",
+  authDomain: "--YOUR--KEY--",
+  projectId: "--YOUR--KEY--",
+  storageBucket: "--YOUR--KEY--",
+  messagingSenderId: "--YOUR--KEY--",
+  appId: "--YOUR--KEY--",
+  measurementId: "--YOUR--KEY--"
 };
 
 // Initialize Firebase
