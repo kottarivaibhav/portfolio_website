43a65a8021a97b284e35583ebf79d3de368f5183
vaibhav kottari <kottarivaibhav@gmail.com>
2024-10-31 10:46:10 +0530

    firbase updated

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -2,14 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+
 const firebaseConfig = {
-  apiKey: "--YOUR--KEY--",
-  authDomain: "--YOUR--KEY--",
-  projectId: "--YOUR--KEY--",
-  storageBucket: "--YOUR--KEY--",
-  messagingSenderId: "--YOUR--KEY--",
-  appId: "--YOUR--KEY--",
-  measurementId: "--YOUR--KEY--"
+  apiKey:"process.env.REACT_APP_API_KEY",
+  authDomain:"process.env.REACT_APP_AUTH_DOMAIN",
+  projectId:"process.env.REACT_APP_PROJECT_ID",
+  storageBucket:"process.env.REACT_APP_STORAGE_BUCKET",
+  messagingSenderId:"process.env.REACT_APP_MESSAGING_SENDER_ID",
+  appId:"process.env.REACT_APP_APP_ID",
+  measurementId:"process.env.REACT_APP_MEASUREMENT_ID"
 };
 
 // Initialize Firebase
